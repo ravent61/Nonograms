e6f0c657bb4c5167bdcfdeba8b0dca2b26e5d991
Aaron J. Smith <onsmith@cs.unc.edu>
2020-11-02 23:36:44 -0500

    Initial commit

---
+++ src/main/java/com/comp301/a08nonograms/view/AppLauncher.java
@@ -0,0 +1,11 @@
+package com.comp301.a08nonograms.view;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class AppLauncher extends Application {
+  @Override
+  public void start(Stage stage) {
+    // TODO: Launch your GUI here
+  }
+}
